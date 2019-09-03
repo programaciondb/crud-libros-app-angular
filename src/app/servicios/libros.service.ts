fd5a3e9474cf52f3fbeb662aaff622cc8cccea7f
David Bousquet <david@programaciondb.cl>
2019-09-03 15:54:38 -0400

    Se agrega metodo put en service

--- src/app/servicios/libros.service.ts
+++ src/app/servicios/libros.service.ts
@@ -9,20 +9,25 @@ import { LibrosInterface } from 'src/app/modelos/libros';
 export class LibrosService {
 
   private rutaRest = "http://localhost:8089/libros/";
-  httpOptions = { headers : new HttpHeaders({"Content-Type" : "application/json"})};
+  httpOptions = { headers: new HttpHeaders({ "Content-Type": "application/json" }) };
   constructor(private http: HttpClient) { }
 
-  public getLibros(){
+  public getLibros() {
     return this.http.get(this.rutaRest);
   }
-  public getLibro(id: number): Observable<LibrosInterface>{
-    return this.http.get<LibrosInterface>(this.rutaRest+id);
+  public getLibro(id: number): Observable<LibrosInterface> {
+    return this.http.get<LibrosInterface>(this.rutaRest + id);
   }
 
   public postLibro(libro: LibrosInterface): Observable<LibrosInterface> {
-      return this.http.post<LibrosInterface>(
-        this.rutaRest, libro, this.httpOptions
-      );
+    return this.http.post<LibrosInterface>(
+      this.rutaRest, libro, this.httpOptions
+    );
+  }
+  public putLibro(id: number, libro: LibrosInterface): Observable<LibrosInterface> {
+    return this.http.post<LibrosInterface>(
+      this.rutaRest + id, libro, this.httpOptions
+    );
   }
 
 }
