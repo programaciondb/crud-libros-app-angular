d39b6c88098d00fc3a60e5c7c4700fdf3ea5b5e1
David Bousquet <david@programaciondb.cl>
2019-09-03 10:36:38 -0400

    Creación app inicial, se crea modelo y se agrega bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LibrosComponent } from './componentes/libros/libros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibrosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/modelos/libros.ts
@@ -0,0 +1,7 @@
+export interface LibroInterface {
+    idlibro: number;
+    titulo: string;
+    autor: string;
+    edicion: number;
+    numcopias: number;
+}
